6010f9efae3d3e897da082c4ea2badf95d53fa55
Tyree Stevenson <tyreeostevenson@gmail.com>
2017-03-17 23:47:05 -0400

    edit graph

--- data_visualizations/generate_visualizations.py
+++ data_visualizations/generate_visualizations.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 def generate_graph_of_highest_scores_by_sports():
     academic_scores_for_sports = pd.read_csv('data_sets/Cumalitive_Academic_Score.csv',index_col='Unnamed: 0')
-    #plt.style.use('dark_background')
-    graph = academic_scores_for_sports.sort(columns='2014_CUMULATIVE_SCORE', ascending=True).plot(kind='bar')
+    graph = academic_scores_for_sports.sort(columns='2014_CUMULATIVE_SCORE', ascending=True).plot(kind='bar',
+                                                                                            width=0.8, figsize=(15, 20))
     graph.set_title("Sports With the Highest Academic Scores")
     graph.set_xlabel("Sports")
     graph.set_ylabel("Cumulative Academic Scores")
-    #print(academic_scores_for_sports['2014_CUMULATIVE_SCORE'])
     plt.savefig("data_visualizations/Highest_Academic_Scores_By_Sport.pdf")
+
 if __name__ == '__main__':
     generate_graph_of_highest_scores_by_sports()
