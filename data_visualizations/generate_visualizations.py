2e510eb6ee3f6bcec3e2eb470521e811867709e5
Tyree Stevenson <tyreeostevenson@gmail.com>
2017-03-22 02:15:41 -0400

    shortened the graphs

--- data_visualizations/generate_visualizations.py
+++ data_visualizations/generate_visualizations.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def generate_graph(file_name, column_name, x_label, y_label, title, figure_name, index_col='Unnamed: 0'):
+def generate_graph(file_name, column_name, x_label, y_label, title, figure_name, rows,index_col='Unnamed: 0'):
     academic_scores_for_sports = pd.read_csv(file_name,index_col='Unnamed: 0')
-    graph = academic_scores_for_sports.sort(columns=column_name, ascending=True).plot(kind='bar',
-                                                                                      width=0.79, figsize=(25, 20),
-                                                                                      linewidth=5)
+    graph = academic_scores_for_sports[rows].sort(ascending=True, columns=rows[0]).plot(kind='bar',
+                                                                                        width=0.79, figsize=(25, 20),
+                                                                                        linewidth=5)
     graph.set_title(title)
     graph.set_xlabel(x_label, fontsize=18)
     graph.set_ylabel(y_label, fontsize=18)
@@ -20,17 +20,21 @@ def generate_graph(file_name, column_name, x_label, y_label, title, figure_name,
 
 
 if __name__ == '__main__':
+    cumaltive_academic_scores_rows = [str(2004 + i) + '_CUMULATIVE_SCORE' for i in range(6, 11)]
+    average_academic_scores_rows = [str(2004 + i) + '_AVERAGE_SCORE' for i in range(6, 11)]
     generate_graph('data_sets/Cumulative_Academic_Score_By_Sport.csv',
                    '2014_CUMULATIVE_SCORE',
                    "Sports",
                    "Cumulative Academic Scores",
                    "Sports With the Highest Academic Scores",
-                   "data_visualizations/Cumulative_Academic_Scores_By_Sport.pdf"
+                   "data_visualizations/Cumulative_Academic_Scores_By_Sport.pdf",
+                   cumaltive_academic_scores_rows
                   )
     generate_graph('data_sets/Average_Academic_Score_By_Sport.csv',
                    '2004_AVERAGE_SCORE',
                    'Sports',
                    'Cumulative Academic Scores',
                    'Average Academic Score For Sport',
-                   'data_visualizations/Average_Academic_Score_By_Sport.pdf'
+                   'data_visualizations/Average_Academic_Score_By_Sport.pdf',
+                    average_academic_scores_rows
                    )
